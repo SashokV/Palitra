9921a7ea22feac868e42c77bf327f06575bcb4bf
SashokV <verozub.s@etoncollege.org.uk>
2025-09-22 08:26:39 +0100

    Initial Commit

---
+++ Palitra/PalitraApp.swift
@@ -0,0 +1,17 @@
+//
+//  PalitraApp.swift
+//  Palitra
+//
+//  Created by Verozub, Sasha (JD) on 22/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct PalitraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PalitraTests/PalitraTests.swift
@@ -0,0 +1,17 @@
+//
+//  PalitraTests.swift
+//  PalitraTests
+//
+//  Created by Verozub, Sasha (JD) on 22/09/2025.
+//
+
+import Testing
+@testable import Palitra
+
+struct PalitraTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
